4d1c01ff64bc7c418a435dd7ebcb528c1770fc3b
prabinmv <prabinmv@live.in>
2023-07-09 12:11:42 +0530

    changed text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacts
+          Learn Reacts & Enjoy
         </a>
       </header>
     </div>
